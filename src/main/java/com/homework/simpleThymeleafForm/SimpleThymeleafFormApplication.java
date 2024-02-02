2ca0cc536ff2f6de46ab2d23712c8a3c6f4c5526
Kotikov <11deadsluts@gmail.com>
2024-02-02 18:07:19 +0300

    Init commit

---
+++ src/main/java/com/homework/simpleThymeleafForm/SimpleThymeleafFormApplication.java
@@ -0,0 +1,13 @@
+package com.homework.simpleThymeleafForm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleThymeleafFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleThymeleafFormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/homework/simpleThymeleafForm/SimpleThymeleafFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homework.simpleThymeleafForm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleThymeleafFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
